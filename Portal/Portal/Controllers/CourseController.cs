80af586019b1eca9d9d1543811b621f4fe90f208
Melody Kossmann <melody.kossmann@s4.co.za>
2021-05-31 20:06:36 +0200

    Added Controllers

---
+++ Portal/Portal/Controllers/CourseController.cs
@@ -0,0 +1,89 @@
+﻿using DevExtreme.AspNet.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NLog;
+using Portal;
+
+namespace VWDMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        DataAccessLayer.DBc.SMSContext db = new DataAccessLayer.DBc.SMSContext();
+
+        [HttpGet]
+        public async Task<IActionResult> Get(DataSourceLoadOptions loadOptions)
+        {
+            var source = db.Courses
+                .Select(p => new DataAccessLayer.DTO.Course
+                {
+                    Id = p.Id,
+                    CourseCode = p.CourseCode,
+                    CourseDesc = p.CourseDesc
+                });
+
+            return Ok(await DataSourceLoader.LoadAsync(source, loadOptions));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] string values)
+        {
+            var Obj = new DataAccessLayer.DBc.Course();
+            JsonConvert.PopulateObject(values, Obj);
+            var checkEntry = db.Courses.Where(c => c.CourseCode == Obj.CourseCode).FirstOrDefault();
+            if(checkEntry != null)
+            {
+                return BadRequest("Course already exists.");
+            }
+            db.Courses.Add(Obj);
+            await db.SaveChangesAsync();
+
+            return Ok(Obj.Id);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromForm] int key, [FromForm] string values)
+        {
+            var Obj = await db.Courses.FirstOrDefaultAsync(o => o.Id == key);
+
+            if (Obj == null) return StatusCode(409, "not found");
+
+            JsonConvert.PopulateObject(values, Obj);
+            var checkEntry = db.Courses.Where(c => c.CourseCode == Obj.CourseCode && c.Id != Obj.Id).FirstOrDefault();
+            if (checkEntry != null)
+            {
+                return BadRequest("Course already exists.");
+            }
+
+            await db.SaveChangesAsync();
+
+            return Ok(Obj.Id);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromForm] int key)
+        {
+            try
+            {
+                var Obj = await db.Courses.FirstOrDefaultAsync(o => o.Id == key);
+                if (Obj == null) return StatusCode(409, "not found");
+
+                db.Courses.Remove(Obj);
+                await db.SaveChangesAsync();
+
+                return Ok("Course deleted");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Course not deleted.");
+            }
+
+        }
+    }
+}
---
+++ Portal/Portal/Controllers/StudentController.cs
@@ -0,0 +1,90 @@
+﻿using DevExtreme.AspNet.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NLog;
+using Portal;
+
+namespace VWDMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        DataAccessLayer.DBc.SMSContext db = new DataAccessLayer.DBc.SMSContext();
+
+        [HttpGet]
+        public async Task<IActionResult> Get(DataSourceLoadOptions loadOptions)
+        {
+            var source = db.Students
+                .Select(p => new DataAccessLayer.DTO.Student
+                {
+                    Id = p.Id,
+                    StudentNr = p.StudentNr,
+                    Name = p.Name,
+                    Surname = p.Surname
+                });
+
+            return Ok(await DataSourceLoader.LoadAsync(source, loadOptions));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromForm] string values)
+        {
+            var Obj = new DataAccessLayer.DBc.Student();
+            JsonConvert.PopulateObject(values, Obj);
+            var checkEntry = db.Students.Where(c => c.StudentNr == Obj.StudentNr).FirstOrDefault();
+            if(checkEntry != null)
+            {
+                return BadRequest("Student already exists.");
+            }
+            db.Students.Add(Obj);
+            await db.SaveChangesAsync();
+
+            return Ok(Obj.Id);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromForm] int key, [FromForm] string values)
+        {
+            var Obj = await db.Students.FirstOrDefaultAsync(o => o.Id == key);
+
+            if (Obj == null) return StatusCode(409, "not found");
+
+            JsonConvert.PopulateObject(values, Obj);
+            var checkEntry = db.Students.Where(c => c.StudentNr == Obj.StudentNr && c.Id != Obj.Id).FirstOrDefault();
+            if (checkEntry != null)
+            {
+                return BadRequest("Student already exists.");
+            }
+
+            await db.SaveChangesAsync();
+
+            return Ok(Obj.Id);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromForm] int key)
+        {
+            try
+            {
+                var Obj = await db.Students.FirstOrDefaultAsync(o => o.Id == key);
+                if (Obj == null) return StatusCode(409, "not found");
+
+                db.Students.Remove(Obj);
+                await db.SaveChangesAsync();
+
+                return Ok("Student deleted");
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Student not deleted.");
+            }
+
+        }
+    }
+}
